d13c514cb27efac16eab2cf45228d5845372963c
revanthe <cenkercetinkya02@gmail.com>
2024-07-12 12:40:25 +0300

    Update detect-player.py

--- detect-player.py
+++ detect-player.py
@@ -1,79 +1,82 @@
-from ultralytics import YOLO
 import streamlit as st
-import moviepy.editor as moviepy
-import os
 import tempfile
+import os
+import moviepy.editor as moviepy
+from ultralytics import YOLO
 
+st.title('Detection of Players at Sports Events')
+best_model_path = r"C:\Users\cenke\Image-process\Ready2Deploy\best.pt"
+model = YOLO(best_model_path)
 
-bestModelpath = '/mount/src/detect-player/best.pt'
-model = YOLO(bestModelpath)
-switch = 0 # Switch caseleri kullanarak diğer ekranlar arası geçiş sağla
+def set_state():
+    if 'stage' not in st.session_state:
+        st.session_state.stage = 0
+    else:
+        st.session_state.stage += 1
+    st.experimental_rerun()
 
+# İlk başta stage'i ayarlayın
+if 'stage' not in st.session_state:
+    st.session_state.stage = 0
 
-if 'button1' not in st.session_state:
-    st.session_state.button1 = False
+if 'uploaded_file' not in st.session_state:
+    st.session_state.uploaded_file = None
 
-def click_button1():
-    st.session_state.button1 = True
+if 'video_path' not in st.session_state:
+    st.session_state.video_path = None
 
-if not st.session_state.button1:
-    st.title("Footballer Detection from Video!")
-    st.button('Start the app.', on_click=click_button1)
-else:
-    uploaded_file = st.file_uploader(r"$\textsf{\Large Upload your video that is a section of any football match!}$", type=["mp4"])
-    if uploaded_file is not None:
-        tfile = tempfile.NamedTemporaryFile(suffix=".mp4", delete=False)
-        tfile.write(uploaded_file.read())
-        video_path = tfile.name
-        st.title("Original Video")
-        st.video(uploaded_file)
-        if 'button2' not in st.session_state:
-            st.session_state.button2 = False           
-        def click_button2():
-            st.session_state.button2 = True                                    
-        if not st.session_state.button2:
-            st.button('Show the processed video', on_click=click_button2)
-        else:
-            video = moviepy.VideoFileClip(video_path)
+if 'directory' not in st.session_state:
+    st.session_state.directory = None
+
+if 'conv_file_path' not in st.session_state:
+    st.session_state.conv_file_path = None
+
+match st.session_state.stage:
+    case 0:
+        if st.button('Upload your video...'):
+            set_state()
+    case 1:
+        st.session_state.uploaded_file = st.file_uploader(r"$\textsf{\Large Upload your video that is a section of any football match!}$", type=["mp4"])
+        if st.session_state.uploaded_file is not None and st.button('Show original video...'):
+            set_state()
+    case 2:
+        if st.session_state.uploaded_file is not None:
+            tfile = tempfile.NamedTemporaryFile(suffix=".mp4", delete=False)
+            tfile.write(st.session_state.uploaded_file.read())
+            st.session_state.video_path = tfile.name
+            st.title("Original Video")
+            st.video(st.session_state.uploaded_file)
+            if st.button('Process the Video...'):
+                set_state()
+    case 3:
+        if st.session_state.video_path is not None and st.session_state.conv_file_path is None:
+            video = moviepy.VideoFileClip(st.session_state.video_path)
             total_frames = int(video.fps * video.duration)
-            progress_bar = st.progress(0, text= "Processing the video. Please wait...")
-            results = model.predict(source=video_path, save=True, project="results", name="predict", stream=True)
-                    
+            progress_bar = st.progress(0, text="Processing the video. Please wait...")
+            results = model.predict(source=st.session_state.video_path, save=True, project="results", name="predict", stream=True)
             frame_id = 0
-            directory = None
             for r in results:
                 if frame_id == 0:
-                    directory = r
+                    st.session_state.directory = r
                 frame_id += 1
                 progress_bar.progress(int((frame_id / total_frames) * 100), text="%" + str(int(((frame_id / total_frames) * 100))))
-                    
             progress_bar.progress(100, text="Done!")
-            video_name = os.path.splitext(os.path.basename(video_path))[0] + '.avi'
-                    
-            if directory is None:
+            video_name = os.path.splitext(os.path.basename(st.session_state.video_path))[0] + '.avi'
+            if st.session_state.directory is None:
                 raise ValueError("Directory does not exist!")
-                    
-            results_path = directory.save_dir
-                    
+            results_path = st.session_state.directory.save_dir
             saved_file_path = os.path.join(results_path, video_name)
             final_video_name = 'finalvideo.mp4'
-            conv_file_path = os.path.join(results_path, final_video_name)
-                    
+            st.session_state.conv_file_path = os.path.join(results_path, final_video_name)
             video2conv = moviepy.VideoFileClip(str(saved_file_path))
-            video2conv.write_videofile(conv_file_path)
-
-            st.title("Final Video")
-                            
-            final_video_path = conv_file_path
-            final_video_file = open(final_video_path, 'rb')
-            final_video_bytes = final_video_file.read()
-                            
-            st.video(final_video_bytes)                    
+            video2conv.write_videofile(st.session_state.conv_file_path)
             st.success('Processing completed!')
- 
-        
-       
-                
-                   
-                            
-            
+        if st.session_state.conv_file_path is not None and st.button('Show the Processed video...'):
+            set_state()
+    case 4:
+        if st.session_state.conv_file_path is not None:
+            st.title("Final Video")
+            final_video_file = open(st.session_state.conv_file_path, 'rb')
+            final_video_bytes = final_video_file.read()
+            st.video(final_video_bytes)
+           
